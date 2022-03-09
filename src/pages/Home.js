7378c6688d254e5610b2df77d3d1daaa8e46383e
ZENIKA <zenika@MacBook-Pro-de-ZENIKA.local>
2022-03-09 16:23:56 +0100

    Home page Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,55 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik"
 import * as Yup from 'yup'
+import styled from 'styled-components'
+import Header from '../components/Header';
+
+const SubmitButton = styled.input`
+    margin-top:100px;    
+    width: 388px;
+    height: 91px;
+    font-family: Lora;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 32px;
+    line-height: 41px;
+    text-align: center;
+
+    background: #EFFF00;
+    border-radius: 30px;
+`
+
+const Form = styled.form`
+text-align:center;
+margin-top:50px;
+`
+
+const Center = styled.div`
+margin-top:50px;
+margin-bot:50px;
+`
+
+
+const TextBox = styled.input`
+width: 623px;
+height: 68px;
+text-align:center;
+background: #FFFFFF;
+border: 1px solid #000000;
+box-sizing: border-box;
+border-radius: 10px;
+`
+
+const Label = styled.label`
+
+font-family: Lora;
+font-style: normal;
+font-weight: bold;
+font-size: 32px;
+text-align:center;
+`
+
+
 
 const Home = () => {
     let navigate = useNavigate();
@@ -27,33 +76,41 @@ const Home = () => {
 
     return (
         <div>
-            <form onClick={formik.handleSubmit}>
-                <div>
-                    <label htmlFor="username" >Username</label>
-                    <input 
-                        name="username"
-                        type="text" 
-                        placeholder="Enter your username"
-                        value={formik.values.username}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.username && <p>{formik.errors.username}</p>}
-                </div>
+            <Header/>
+            <Form onClick={formik.handleSubmit}>
+                <Center>
+                    <div>
+                        <Label htmlFor="username" >Username</Label>
+                    </div>
+                    <div>
+                        <TextBox 
+                            name="username"
+                            type="text" 
+                            placeholder="Enter your username"
+                            value={formik.values.username}
+                            onChange={formik.handleChange}
+                        />
+                        {formik.errors.username && <p>{formik.errors.username}</p>}
+                    </div>
+                </Center>
+                <Center>
+                    <div>
+                        <Label htmlFor="password" >Password</Label>
+                    </div>
+                    <div>
+                        <TextBox 
+                            name="password"
+                            type="password" 
+                            placeholder="Enter your password"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                        />
+                        {formik.errors.password && <p>{formik.errors.password}</p>}
+                    </div>
+                </Center>
+                <SubmitButton type="submit" value="Login"></SubmitButton>
                 
-                <div>
-                    <label htmlFor="password" >Username</label>
-                    <input 
-                        name="password"
-                        type="text" 
-                        placeholder="Enter your password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.password && <p>{formik.errors.password}</p>}
-                </div>
-                
-                <input type="submit" value="Login" />
-            </form>
+            </Form>
         </div>
     );
 };
